3fffacdd8ac3dd4e168b6f833749680afc2cbc3c
pietro573 <pietro.czerski@escola.pr.gov.br>
2026-05-29 08:59:52 -0300

    Update telabranco.py

--- aula10/telabranco.py
+++ aula10/telabranco.py
@@ -1,15 +1,19 @@
-from conta import conta
-conta1 = conta("pietro", 1234, "1208-7")
+
+from conta import Conta
+conta1 = Conta("Pietro", 1234, "1010-7", "111.111.111-11")
 conta1.extrato()
 conta1.deposito(1000)
-conta1.extrato()
 conta1.saque(200)
 conta1.saque(1500)
 conta1.extrato()
-conta2 = conta('richard', 1234, '7070-7')
+conta2 = Conta("richaard", 1234, "7070-7", "999.999.999-99")
 conta2.extrato()
 conta1.transferir(300, conta2)
 conta1.extrato()
 conta2.extrato()
-conta2.titular = 'richard'
-print(f'ola, {conta2.titular}! :')
+conta2.titular = "Richard"
+print(f'''Olá, {conta2.titular}! :)
+Agência: {conta2.agencia} Conta: {conta2.numero}
+Saldo: {conta2.saldo}
+Senha: {conta2.senha}
+Chaves pix: {conta2.chavepix}''')
