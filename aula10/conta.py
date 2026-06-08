2bae2999791e105487738bd4cb511912312dde9a
pietro573 <pietro.czerski@escola.pr.gov.br>
2026-06-08 09:44:00 -0300

    Update conta.py

--- aula10/conta.py
+++ aula10/conta.py
@@ -1,55 +1,60 @@
-class conta:
-    # metodo construtor 
-    def __init__(self, titular, agencia, numero):
+import random
+class Conta:
+    # método construtor
+    def __init__(self, titular, agencia, cpf):
         self.__titular = titular
         self.__agencia = agencia
-        self.__numero = f"{numero.randint(1000, 9999)}-{ random.randint(1,9)}"
+        self.__numero = f"{random.randint(1000, 9999)}-{random.randint(1,9)}"
         self.__cpf = cpf
         self.__saldo = 0
-        self.__senha = random.randint(180000, 99999999)
+        self.__senha = random.randint(100000, 999999)
         self.__chavepix = []
-
-        @property
-        def titular(self)
-        return(self.__titular)
-
-         @titular.setter
-        def titular(self, novo_nome)
+    
+    #encapsulamento (getters e setters)
+    @property
+    def titular(self):
+        return self.__titular
+    @titular.setter
+    def titular(self, novo_nome):
         self.__titular = novo_nome
+    @property
+    def agencia(self):
+        return self.__agencia
+    @property
+    def numero(self):
+        return self.__numero
+    @property
+    def cpf(self):
+        return self.__cpf
+    @property
+    def saldo(self):
+        return self.__saldo
+    @property
+    def chavepix(self):
+        return self.__chavepix
+    @property
+    def senha(self):
+        return self.__senha
+    
 
-         @property
-        def agencia(self)
-        return(self.__agencia)
-
-         @property
-        def numero(self)
-        return(self.__numero)
-
-         @property
-        def saldo(self)
-        return(self.__saldo)
-
-         @property
-        def cpf(self)
-        return(self.__cpf)
-
-         @property
-        def chavepix(self)
-        return(self.__chavepix)
-
-
+    # métodos da classe
     def extrato(self):
-        print(f'o saldo do { self.__titular} é {self.__saldo}')
+        print(f"O saldo da {self.__titular} é {self.__saldo}")
 
     def deposito(self, valor):
-        self.__saldo = self.__saldo + valor
+        if valor > 0:
+            self.__saldo = self.__saldo + valor
+            print("Depósito efetuado com sucesso!")
+        else:
+            print("Não foi possível depositar!")
 
-        def saque(self, valor)
-        if valor <= self.__saldo and valor >0:
+    def saque(self, valor):
+        if valor <= self.__saldo and valor > 0:
             self.__saldo = self.__saldo - valor
-            print('saque efetuado com sucesso')
-            else:
-                print('erro ao efetuar o saque')
-                def transferir(self, conta_destino, valor):
-            self.__saque(valor)
-            conta_destino.destino(valor)
+            print("Saque efetuado com sucesso!")
+        else:
+            print("Erro ao efetuar saque")
+
+    def transferir(self, valor, conta_destino):
+        self.saque(valor)
+        conta_destino.deposito(valor)
